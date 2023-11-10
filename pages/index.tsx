57b61788a84c9d553feb58cbf77c5a215af3c7ef
Mike Kruk <mike@ripeworks.com>
2023-11-10 11:49:42 -0500

    added verifyInstallation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { View, Text } from "react-native";
+import { verifyInstallation } from "nativewind";
 
 export default function Home() {
+  verifyInstallation();
+
   return (
     <View className="bg-blue-200 rounded-2xl p-6">
       <Text className="text-white">Welcome to next.js</Text>
