55bef8222f53bba33f36ec30e1f25f18e0dbde30
Mike Kruk <mike@ripeworks.com>
2023-11-10 11:41:42 -0500

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      "react-native$": "react-native-web",
+      "react-native-svg": "react-native-svg-web",
+    };
+    config.resolve.extensions = [
+      ".web.js",
+      ".web.ts",
+      ".web.tsx",
+      ...config.resolve.extensions,
+    ];
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
